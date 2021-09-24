dcb12fc4a928ba8a7b1075f3b518c5afda1d4d39
Edwin Rico Carandang <edwinrn.carandang@yahoo.com>
2021-09-24 14:23:50 -0500

    update route

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -5,7 +5,7 @@ import { SideNavItems, SideNavLink } from 'carbon-components-react/lib/component
 import { StyledSideNav } from './styles';
 
 const items = [
-  { name: 'Me', path: '/' },
+  { name: 'Me', path: '/PR300-Phase2ReactPortfolio' },
   { name: 'Projects', path: '/projects' },
   { name: 'Work', path: '/work' },
   { name: 'Education', path: '/education' },
